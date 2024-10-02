1025b291108bb6f86aa0e06cd96f7fd68f873b20
kairi003 <kairi.satellite@gmail.com>
2024-10-02 23:57:53 +0900

    [WIP]

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 import datetime as dt
+from itertools import count
 import os
 import re
 import sys
 from distutils.util import strtobool
 from logging import basicConfig, getLogger
 
+from playwright.sync_api._generated import Route
 from retry import retry
 from playwright.sync_api import Page, sync_playwright
 from register import register_event
@@ -23,21 +25,6 @@ logger = getLogger(__name__)
 HEADLESS = bool(strtobool(os.getenv("HEADLESS", "true")))
 
 
-def close_dialog(page: Page):
-    page.evaluate('''() => {
-        [...document.querySelectorAll('.native-text')]
-        .find(n=>n.textContent==JSON.parse('"\\udb86\\udd05"'))?.click()
-    }''')
-
-
-def load_more(page: Page):
-    child_num = len(page.query_selector_all('[data-successful-render-id]>*'))
-    page.evaluate('''() => {
-        window.scrollTo(0, document.body.scrollHeight);
-    }''')
-    page.wait_for_selector(
-        f'[data-successful-render-id]>:nth-child({child_num+10})')
-
 
 def update_event_log(event_date: dt.date) -> bool:
     with open(EVENT_LOG, 'r+') as f:
@@ -53,19 +40,6 @@ def update_event_log(event_date: dt.date) -> bool:
         else:
             logger.info('event already registered')
 
-@retry(tries=5, delay=5)
-def get_navite_text_elements(page: Page):
-    # goto page and wait for load
-    
-    page.goto(PAGE_URL, wait_until="domcontentloaded")
-    page.wait_for_selector('[data-successful-render-id]>:nth-child(30)', timeout=15000)
-    logger.debug('page loaded')
-
-    # get text elements
-    navite_text_elements = page.query_selector_all(
-        '[data-successful-render-id] .native-text')
-    
-    return navite_text_elements
 
 
 def main():
@@ -75,53 +49,24 @@ def main():
         browser = p.chromium.launch(headless=HEADLESS)
         context_params = {
             'user_agent': USER_AGENT,
-            'locale': 'ja-JP'
+            'locale': 'ja-JP',
+            'viewport': { 'width': 500, 'height': 5000 }
         }
         context = browser.new_context(**context_params)
+        context.route("**/*", lambda route: route.abort() if route.request.resource_type in "image" else route.continue_())
         page = context.new_page()
         logger.debug('start page')
 
-        post_dt = None
-        for element in get_navite_text_elements(page):
-            text = element.text_content()
+        page.goto(PAGE_URL, wait_until="domcontentloaded")
+
+        page.wait_for_selector('[aria-label="閉じる"]').click()
+
+        for i in range(1, 20):
+            selector = f'[aria-posinset="{i}"] [data-ad-preview="message"]'
+            el = page.wait_for_selector(selector)
+            text = el.text_content()
             logger.debug([text])
-
-            # post date element
-            m: re.Match
-            if m := re.match(r'^(?P<num>\d+)(?P<unit>秒|分|時間|日)前..$', text):
-                post_dt = dt.datetime.now(JST)
-                num = int(m['num'])
-                match m['unit']:
-                    case '秒':
-                        post_dt -= dt.timedelta(seconds=num)
-                    case '分':
-                        post_dt -= dt.timedelta(minutes=num)
-                    case '時間':
-                        post_dt -= dt.timedelta(hours=num)
-                    case '日':
-                        post_dt -= dt.timedelta(days=num)
-                continue
-
-            elif m := re.match(r'^(?P<year>\d+)/(?P<month>\d+)/(?P<date>\d+)..$', text):
-                date_ = map(int, m.group('year', 'month', 'date'))
-                post_dt = dt.datetime(*date_, tzinfo=JST)
-                continue
-
-            # text element
-            elif post_dt is not None:
-                logger.info(f'{post_dt.isoformat()}: {text}')
-
-                # event post text
-                if m := re.search(r'(?P<month>\d+)月(?P<date>\d+)日.*室料半額', text):
-                    month, date_ = map(int, m.group('month', 'date'))
-                    event_date = dt.date(post_dt.year, month, date_)
-                    if event_date < post_dt.date():
-                        event_date = dt.date(post_dt.year+1, month, date_)
-                    logger.info(event_date)
-                    update_event_log(event_date)
-                    break
-        else:
-            raise Exception('event not found')
+            el.evaluate('el=>el.scrollIntoView(true)')
 
         browser.close()
 
