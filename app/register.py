0ffa8c26cfa06c1250a53b190f3302576e8add30
kairi003 <kairi.satellite@gmail.com>
2024-10-03 04:22:06 +0900

    Update: gcp auth

--- app/register.py
+++ app/register.py
@@ -5,6 +5,9 @@ import sys
 from logging import basicConfig, getLogger
 from typing import Any
 
+from google.auth import load_credentials_from_file
+from google.auth.credentials import TokenState
+from google.auth.exceptions import GoogleAuthError
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -19,33 +22,34 @@ CLIENT_SECRET = "credentials.json"
 SCOPES = ["https://www.googleapis.com/auth/calendar.events.owned"]
 
 
-def get_creds() -> Credentials:
-    """Shows basic usage of the Google Calendar API.
-    Prints the start and name of the next 10 events on the user's calendar.
-    """
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists(USER_TOKEN):
-        creds = Credentials.from_authorized_user_file(USER_TOKEN, SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                CLIENT_SECRET, SCOPES
-            )
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open(USER_TOKEN, "w") as token:
-            token.write(creds.to_json())
+def get_credentials() -> Credentials:
+    """Get the credentials for the Google Calendar API."""
+
+    # For google-github-actions/auth
+    if creds_path := os.environ.get("GOOGLE_GHA_CREDS_PATH"):
+        creds, project_id = load_credentials_from_file(creds_path, scopes=SCOPES)
+        creds.refresh(Request())
+        return creds
+    
+    # For Local Development
+    try:
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+        if creds.token_state is TokenState.FRESH:
+            # return the credentials if it is fresh
+            return creds
+        creds.refresh(Request())
+    except (GoogleAuthError, FileNotFoundError):
+        # If there are no (valid) credentials available, let the user log in.
+        flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+        creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open("token.json", "w") as token:
+        token.write(creds.to_json())
     return creds
 
 
 def register_event(event_date: dt.date) -> Any:
-    creds = get_creds()
+    creds = get_credentials()
     service = build("calendar", "v3", credentials=creds)
     data = {
         "summary": EVENT_TITLE,
@@ -55,6 +59,3 @@ def register_event(event_date: dt.date) -> Any:
     event = service.events().insert(calendarId=CALENDAR_ID, body=data).execute()
     return event
 
-
-if __name__ == "__main__":
-    get_creds()
